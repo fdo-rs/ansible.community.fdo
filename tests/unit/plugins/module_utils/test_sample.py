d77fb969158ded90df41ddb4f3dbf9e1d76042fc
matevzerzen <jst.matevz@gmail.com>
2022-12-12 14:52:16 +0100

    Added test playbooks and licenses

--- tests/unit/plugins/module_utils/test_sample.py
+++ tests/unit/plugins/module_utils/test_sample.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright: (c) 2022, XLAB Steampunk <steampunk@xlab.si>
-#
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
@@ -14,11 +13,9 @@ pytestmark = pytest.mark.skipif(
     sys.version_info < (2, 7), reason="requires python2.7 or higher"
 )
 
-
 class TestHello:
     def test_hello(self):
         assert True
-
 
 def test_hello():
     assert True
